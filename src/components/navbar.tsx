792edfa295a3e02151ddd1343e1e682a667de3d9
malvin <malvinmixamo@gmail.com>
2025-01-24 20:09:25 +0700

    wea

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image"
 export default function Nav(){
     return(
-        <div className=" bg-white/35 backdrop-blur">
+        <div className=" bg-purple-950/5 backdrop-blur-md">
             <Image width={50} height={50} src="/logoTC.png" alt="" className=" "></Image>
             <div className=" ">
-
+                
             </div>
         </div>
     )
