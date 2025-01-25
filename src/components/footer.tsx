3f2e530dbdf0c0f1201be8335bd2844f0354f1d7
malvin <malvinmixamo@gmail.com>
2025-01-25 19:33:53 +0700

    im

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,8 @@
+import Image from "next/image"
 export default function Footer(){
     return(
         <div>
-            
+            <Image width={30} height={30} src="/logoTC.png" alt=""></Image>
         </div>
     )
 }
