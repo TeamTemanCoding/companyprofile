9993b6b18c7d0944f9bff09c68a2f2dec91058ca
malvin <malvinmixamo@gmail.com>
2025-01-25 08:58:12 +0700

    miaw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar"
+import Footer from "../components/footer"
 
 const PoppinsSans = Poppins({
   weight: ['400', '600', '700'], 
@@ -27,6 +28,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
