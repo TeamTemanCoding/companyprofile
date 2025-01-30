1c56d97c6a3788bf6f0bdced0cd6bd13fef11e8b
iqblfrdsyh <iqblfrdsyh@gmail.com>
2025-01-30 21:00:38 +0700

    chore(animation) : make spin animation speed dynamic with props

--- src/components/sections/visionMission.tsx
+++ src/components/sections/visionMission.tsx
@@ -6,8 +6,8 @@ const VisionMission = () => {
         <React.Fragment>
             <Box label="Vision" description="  To be a trusted team in creating creative and innovative digital
               solutions through high-quality websites that support business and
-              technology development."/>
-            <Box label="Mission" description="Providing the best service, developing the skills of each team member, building long-term relationships with clients, focusing on professional results." />
+              technology development." className='animate-[spin_6s_linear_infinite] max-sm:animate-[spin_7s_linear_infinite]'/>
+            <Box label="Mission" description="Providing the best service, developing the skills of each team member, building long-term relationships with clients, focusing on professional results." className='animate-[spin_7s_linear_infinite] max-sm:animate-[spin_8s_linear_infinite]' />
         </React.Fragment>
     )
 }
